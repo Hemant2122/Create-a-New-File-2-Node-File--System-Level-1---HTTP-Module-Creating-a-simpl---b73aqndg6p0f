2cf8559db3a654d0cee7d2e12cd458fe7b0382c4
Hemant kumar <hemantkumarsonkar1@gmail.com>
2024-06-01 16:33:07 +0530

    Create a New File-2 : Node File- System Level 1

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const fileContent = "Newton School";
 const writeFile = async (fileName, fileContent) => {
     //Write your code here
     //Don't change function name
-  
+    await fs.writeFile(fileName, fileContent);
 };
+writeFile(fileName, fileContent)
+.then((data) => console.log("File myfile.txt created and data written successfully!"))
+.catch((error) => console.log("myfile.txt already exists. Skipping write operation"))
 module.exports =  writeFile ;
